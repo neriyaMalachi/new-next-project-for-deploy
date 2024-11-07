149fa904772df6f59031f3a544dc26986f911591
Neriya Malachi <neriamalachi@gmail.com>
2024-11-07 12:55:36 +0200

    first2

---
+++ src/app/pages/apiuser/page.tsx
@@ -0,0 +1,23 @@
+// import { postapi } from "@/app/services/userAction";
+// import React from "react";
+// import CardForApi from "@/app/components/CardForApi";
+// import PostApiInderface from "@/app/types/PostApi";
+
+const page = async () => {
+  // const data = await postapi();
+  // const posts: PostApiInderface[] = data.data;
+  // console.log(posts);
+  
+  return (
+    <div>
+      <h1>API USERS</h1>
+      {/* {posts.map((user) => (
+        <div className="" key={user.id}>
+          <CardForApi id={user.id} body={user.body} title={user.title} />
+        </div>
+      ))} */}
+    </div>
+  );
+};
+
+export default page;
