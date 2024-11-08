1fe1cdbfea1b2ab6dfa9d8384455db350fd5e96f
Neriya Malachi <neriamalachi@gmail.com>
2024-11-08 10:47:35 +0200

    commit 1 friday

--- src/app/pages/users/page.tsx
+++ src/app/pages/users/page.tsx
@@ -1,11 +1,11 @@
-// import UserComponent from '@/app/components/UserComponent'
+import UserComponent from '@/app/components/UserComponent'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
         <h1>USERS</h1>
-        {/* <UserComponent/> */}
+        <UserComponent/>
     </div>
   )
 }
